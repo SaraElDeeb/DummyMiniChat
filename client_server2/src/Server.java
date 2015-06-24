f493f6c127cd09c42264babdc0d0cfa63463a1f8
Baron Leonardo <mohamedayman.fcis@zoho.com>
2015-06-24 13:58:55 +0200

    reconstruct the Server to construct a thread for every connection

--- client_server2/src/Server.java
+++ client_server2/src/Server.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -6,56 +7,111 @@ public class Server
 {
     private byte[] IP;
     private int port = 0;
-    Server(byte[] IP, int port )
+    int max_num_connections = 1;
+
+    Server(byte[] IP, int port, int max_num_connections)
     {
         set_IP(IP);
         set_port(port);
+        set_max_number_of_connection(max_num_connections);
     }
 
-    void set_port(int port)
-    {
+    void set_port(int port) {
         this.port = port;
     }
 
-    void set_IP(byte[] IP)
-    {
+    void set_IP(byte[] IP) {
         this.IP = IP;
     }
 
-    boolean initiate() throws IOException
-    {
-        ServerSocket server = new ServerSocket(this.port);
+    void set_max_number_of_connection(int max_num_connections) {
+        this.max_num_connections = max_num_connections;
+    }
 
-        try
-        {
+    void initiate() throws IOException {
+        InetAddress address = InetAddress.getByAddress(this.IP);
+
+        try (ServerSocket server = new ServerSocket(this.port, this.max_num_connections, address)) {
             while (true)
             {
                 // Listen
                 Socket clientSocket = server.accept();
-                // Read Stream
-                BufferedReader in = null;
-                // Write Stream
-                PrintWriter out = null;
 
-                if(server.isBound())
+                if (server.isBound())
                 {
-                    try
-                    {
-                        System.out.println("IP address: " + server.getLocalSocketAddress());
-                        out = new PrintWriter(clientSocket.getOutputStream(), true);
-                        out.println("This a welcome message.");
-
-                        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-                    } finally {
-                        clientSocket.close();
-                        in.close();
-                        out.close();
-                    }
+                    Communication communication = new Communication(clientSocket);
+                    communication.start_communication();
                 }
             }
-        } finally {
-            server.close();
+        }
+    }
+}
+
+
+class Communication implements Runnable
+{
+    Socket client_socket = null;
+    Thread thread = null;
+
+    Communication(Socket client_socket)
+    {
+        this.client_socket = client_socket;
+        this.thread = new Thread(this);
+    }
+
+    void start_communication()
+    {
+        thread.start();
+    }
+
+    @Override
+    public void run()
+    {
+        PrintWriter out = null;
+        try {
+            out = new PrintWriter(this.client_socket.getOutputStream(), true);
+            out.println("This a welcome message.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(this.client_socket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String str = "";
+
+        while (( !str.equals("EOL") || !this.client_socket.isClosed() ) && in != null)
+        {
+            try
+            {
+                str = in.readLine();
+                System.out.println(str);
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+
+        try
+        {
+            if (in != null && out != null )
+            {
+                in.close();
+                out.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try
+        {
+            client_socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
