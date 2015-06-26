1b097b9c671b285e9f678ecc98d12bfa0e9c12ca
Baron Leonardo <mohamedayman.fcis@zoho.com>
2015-06-26 21:45:31 +0200

    move main() inside the server class

--- client_server2/src/Server.java
+++ client_server2/src/Server.java
@@ -100,4 +100,16 @@ public class Server implements Observer
         Socket socket = (Socket)o;
         list_of_sockets.remove(socket);
     }
+
+    public static void main(String[] args) throws IOException
+    {
+        byte[] IP = {127,0,0,1};
+        int port = 12345;
+        int max_num_connections = 1;
+
+        Server server = new Server(IP, port, max_num_connections);
+        server.set_welcome_message("This is a welcome message.");
+        server.set_message_on_new_connection("", true);
+        server.run_server();
+    }
 }
