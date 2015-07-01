44ade3ed9291222bea80ff4a59bc746ce98ed8c3
SaraElDeeb <saraadel.fcis2012>
2015-07-01 17:53:36 +0100

    create a client

---
+++ client_server1/mini chat/mini chat/Client.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace mini_chat
+{
+    class Client
+    {
+        string ip;
+        int port;
+        TcpClient client;
+
+        public Client()
+        {
+            client = new TcpClient();
+        }
+
+        public bool connect(string ip, int port)
+        {
+            this.ip = ip;
+            this.port = port;
+
+            try
+            {
+                client.Connect(this.ip, this.port);
+                return true;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("error.." + ex.StackTrace);
+                return false;
+                
+            }
+            
+            
+
+
+        }
+    }
+}
---
+++ client_server1/mini chat/mini chat/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mini_chat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ip = "127.0.0.1";
+            int port = 12345;
+            Client clnt = new Client();
+
+            bool result = clnt.connect(ip, port);
+
+            if (result)
+                Console.WriteLine("connected");
+             
+        }
+    }
+}
